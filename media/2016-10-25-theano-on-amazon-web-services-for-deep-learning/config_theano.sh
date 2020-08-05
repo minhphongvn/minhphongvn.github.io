336544705d4f1ae8b1748bea70d8d6c3a22889a6
Phong Nguyen <minhphong375@gmail.com>
2020-08-05 18:28:57 +0700

    hehe

--- media/2016-10-25-theano-on-amazon-web-services-for-deep-learning/config_theano.sh
+++
@@ -1,9 +0,0 @@
-#  set up the theano config file to use gpu by default
-{
-    echo -e "\n[global]\nfloatX=float32\ndevice=gpu"
-    echo -e "[mode]=FAST_RUN"
-    echo -e "\n[nvcc]"
-    echo -e "fastmath=True"
-    echo -e "\n[cuda]"
-    echo -e "root=/usr/local/cuda" 
-}>> ~/.theanorc
